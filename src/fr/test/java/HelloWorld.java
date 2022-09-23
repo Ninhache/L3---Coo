819b191997f9b1460b541f975d2056844e8f882a
Néo ALMEIDA <neo.almeida.etu@univ-lille.fr>
2022-09-23 09:32:20 +0200

    feat: Add the first unimplemented test

---
+++ src/fr/test/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package fr.test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorld {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
