e40f2e1a5a62953930274fedbc33f48edbae1213
Ambre Boussert <ambre.boussert.etu@univ-lille.fr>
2022-10-01 15:03:10 +0200

    :sparkles: match : rigged,weighted,random

---
+++ src/fr/main/java/match/Match.java
@@ -0,0 +1,15 @@
+package fr.main.java.match;
+
+import java.util.List;
+
+public abstract class Match {
+	private List<Competitor> competitors;
+	
+	public Match(List<Competitor>) {
+		
+	}
+	
+	public Competitor playMatch() {
+		
+	}
+}
