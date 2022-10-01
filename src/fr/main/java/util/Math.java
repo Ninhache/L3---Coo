86aae5efe73293b169496f845e81c82ab939fd21
Neo <neo.almeida.etu@univ-lille.fr>
2022-10-01 15:47:49 +0200

    feat: add doc

--- src/fr/main/java/util/Math.java
+++ src/fr/main/java/util/Math.java
@@ -2,6 +2,12 @@ package fr.main.java.util;
 
 public class Math {
 
+	/**
+	 *	Return true if the number passed in parameter is a power of two
+	 * 
+	 * @param number
+	 * @return
+	 */
 	public static boolean isPowerOfTwo(int number) {
 		return (number != 0) && ((number & (number - 1)) == 0);
 	}
