dbf4714debdd445e73ccfab5b84161e0b770d212
Neo <neo.almeida.etu@univ-lille.fr>
2022-10-01 16:05:40 +0200

    fix: constructor / fields wasnt initialized

--- src/fr/main/java/Competitor.java
+++ src/fr/main/java/Competitor.java
@@ -1,12 +1,15 @@
 package fr.main.java;
 
 public class Competitor implements Comparable {
-	int wins;
+
+	private String name;
+	private int wins;
 	
 	public Competitor(String name) {
-		
+		this.name = name;
+		this.wins = 0;
 	}
-	
+
 	public void addWin() {
 		
 	}
@@ -16,4 +19,21 @@ public class Competitor implements Comparable {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getWins() {
+		return wins;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+	
 }
