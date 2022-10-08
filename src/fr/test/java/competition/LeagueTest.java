b2e8119e3c76a24c58e35fb157e069807f0de815
Ambre Boussert <ambre.boussert.etu@univ-lille.fr>
2022-10-08 22:38:59 +0200

    :bug: fix test

--- src/fr/test/java/competition/LeagueTest.java
+++ src/fr/test/java/competition/LeagueTest.java
@@ -10,25 +10,26 @@ import org.junit.Test;
 import fr.main.java.Competitor;
 import fr.main.java.competition.League;
 import fr.main.java.exceptions.CompetitionIllegalCompetitorsSize;
+import fr.main.java.match.RandomMatch;
 
 public class LeagueTest extends CompetitionTest {
 	
 	@Test
 	public void testRightCountOfMatches() throws CompetitionIllegalCompetitorsSize {
+		RandomMatch match = new RandomMatch();
 		List<Competitor> competitors = new ArrayList<>();
 		for(int i=0;i<3;i++) {
 			competitors.add(new Competitor("Player"+i));
 		}
-		League l1 = new League(competitors);
+		League l1 = new League(competitors, match);
 		l1.play();
 		
-		assertEquals(9,l1.getNbMatch());
+		assertEquals(6,l1.getNbMatch());
 		
 		competitors.add(new Competitor("Player4"));
-		League l2 = new League(competitors);
+		League l2 = new League(competitors, match);
 		l2.play();
 		
-		assertEquals(12,l1.getNbMatch());
-		
+		assertEquals(12,l2.getNbMatch());
 	}
 }
