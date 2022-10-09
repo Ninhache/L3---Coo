9bb53fa798bf02463cfba997c98c4f532837f4ed
Ambre Boussert <ambre.boussert.etu@univ-lille.fr>
2022-10-09 12:04:30 +0200

    ✅ tdd Tournament : constructors

--- src/fr/main/java/competition/Tournament.java
+++ src/fr/main/java/competition/Tournament.java
@@ -3,11 +3,21 @@ package fr.main.java.competition;
 import java.util.List;
 
 import fr.main.java.Competitor;
+import fr.main.java.exceptions.CompetitionIllegalCompetitorsSize;
+import fr.main.java.exceptions.TournamentIllegalCompetitorsSize;
+import fr.main.java.match.AbstractMatch;
+import fr.main.java.util.Math;
 
 public class Tournament extends Competition {
 	
-	public Tournament(List<Competitor> competitors) {
+	public Tournament(List<Competitor> competitors,AbstractMatch match) throws TournamentIllegalCompetitorsSize, CompetitionIllegalCompetitorsSize {
 		super(competitors);
+		if(!Math.isPowerOfTwo(competitors.size()))
+			new TournamentIllegalCompetitorsSize();
+	}
+	
+	public Tournament(List<Competitor> competitors) throws CompetitionIllegalCompetitorsSize, TournamentIllegalCompetitorsSize {
+		this(competitors,null);
 	}
 
 	@Override
